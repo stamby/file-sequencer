1f2118b9af08ae267a60e72a8f59d3d7d2da2187
Bruno Molteni <34241947+stamby@users.noreply.github.com>
2019-06-06 11:45:44 -0300

    Update file-sequencer.sh

--- file-sequencer.sh
+++ file-sequencer.sh
@@ -11,7 +11,7 @@ Rename the given FILES to a sequence of numbers, as per FORMAT.
 FORMAT should be written the way it is with 'printf', leaving room for a single
 number to appear in the file names. For instance, supposing you have 100 files:
 
-"%d.txt" to name files 1.txt, 2.txt, [...] 100.txt
+"%d.txt" to name them 1.txt, 2.txt, [...] 100.txt
 "DSC_%04d.JPG" to name them DSC_0001.JPG, DSC_0002.JPG, [...] DSC_0100.JPG
 
 DIRECTORY is where the renamed FILES will be copied, will be created if non
